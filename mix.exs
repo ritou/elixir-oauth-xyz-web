f806eea7df0d4c6d8a898057877e413088ebbfb0
ritou <ritou.06@gmail.com>
2019-12-05 03:55:25 +0900

    update mix.exs for initial runnning

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule OAuthXYZ.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:plug_cowboy, "~> 1.0"}
     ]
   end
 
@@ -54,7 +55,7 @@ defmodule OAuthXYZ.Mixfile do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      "test": ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
