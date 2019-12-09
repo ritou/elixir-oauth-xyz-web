20f85b624681f0bb072aa7db00a13fdf2524115d
ritou <ritou.06@gmail.com>
2019-12-10 02:51:52 +0900

    add routing

--- lib/oauth_xyz_web/router.ex
+++ lib/oauth_xyz_web/router.ex
@@ -2,25 +2,41 @@ defmodule OAuthXYZWeb.Router do
   use OAuthXYZWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", OAuthXYZWeb do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    # for top page
+    get("/", PageController, :index)
+
+    # for interaction
+    get("/interact", InteractionController, :get_interaction)
+    post("/interact/approve", InteractionController, :post_interaction_approve)
+
+    # for device
+    get("/interact/device", InteractionController, :get_interaction_device)
+    post("/interact/device", InteractionController, :post_interaction_device)
+
+    # for user signin session
+    get("/signin", SessionController, :get_signin)
+    post("/signin", SessionController, :post_signin)
+    post("/signout", SessionController, :post_signout)
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", OAuthXYZWeb do
-  #   pipe_through :api
-  # end
+  scope "/api", OAuthXYZWeb do
+    pipe_through(:api)
+
+    post("/transaction", TransactionController, :post_transaction)
+  end
 end
